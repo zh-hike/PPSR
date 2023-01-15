9850116b1926cd48176419956694ff1a5bbd5a13
zh-hike <1583124882@qq.com>
2023-01-15 22:38:28 +0800

    增加seed,amp,修复运行中断bug

--- utils/util.py
+++ utils/util.py
@@ -2,13 +2,15 @@ import paddle
 from PIL import Image
 import os
 import io
+import numpy as np
+import random
 
 
 def quantize(img, rgb_range=1):
     img = paddle.clip(255/rgb_range*img, 0, 255).astype('float32')
     return img
 
-def read_img(file):
+def read_img(file, return_pil=True):
     img = Image.open(file)
     return img
 
@@ -16,5 +18,12 @@ def read_img(file):
     # with open(file, 'rb') as f:
     #     data = io.BytesIO(f.read())
     #     img = Image.open(data)
+    #     img_array = np.asarray(img)
 
+    # img = Image.fromarray(img_array) if return_pil else img_array
     # return img
+
+def set_seed(seed):
+    paddle.seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
